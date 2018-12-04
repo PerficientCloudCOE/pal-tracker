45d39ed31608524fde25bbe8f6d4c814ed30f0d0
rajaganesh88 <R.Rajasekaran@perficient.com>
2018-12-04 10:59:57 +0530

    Added manifest and cf scale

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class WelcomeController {
+    @Value("${WELCOME_MESSAGE}")
+    public String message;
+    public WelcomeController(@Value("${WELCOME_MESSAGE:NOT SET}") String msg){
+        this.message = msg;
+
+    }
     @GetMapping("/")
+
     public String sayHello() {
-        return "hello";
+        return message;
     }
 
 }
